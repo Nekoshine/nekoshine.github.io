deaa294f1b1e7297c3f97dd96da60ca4802eff45
Nekoshine <uharry44@gmail.com>
2021-02-17 12:05:07 +0100

    Coord Worker
    test1

---
+++ workerCoord.js
@@ -0,0 +1,7 @@
+onmessage = function() {
+  console.log('Worker: Message received from main script');
+  const workerResult = 'Coord: ' + getRandomInt(200);
+  console.log('Worker: Posting message back to main script');
+  postMessage(workerResult);
+}
+}
